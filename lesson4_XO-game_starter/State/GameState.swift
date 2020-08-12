e3898da0b62bcd92e692a7e0c588486667540aed
Oksana_Zvereva <zvereva.oksana@gmail.com>
2020-08-12 21:10:09 +0500

    переключение игроков, определение победителя

---
+++ lesson4_XO-game_starter/State/GameState.swift
@@ -0,0 +1,16 @@
+//
+//  GameState.swift
+//  XO-game
+//
+//  Created by Оксана Зверева on 12.08.2020.
+//  Copyright © 2020 plasmon. All rights reserved.
+//
+
+public protocol GameState {
+    
+    var isCompleted: Bool { get }
+    
+    func begin()
+    
+    func addMark(at position: GameboardPosition)
+}
--- lesson4_XO-game_starter/XO-game/GameViewController.swift
+++ lesson4_XO-game_starter/XO-game/GameViewController.swift
@@ -16,16 +16,53 @@ class GameViewController: UIViewController {
     @IBOutlet var winnerLabel: UILabel!
     @IBOutlet var restartButton: UIButton!
     
+    private let gameboard = Gameboard()
+    private var currentState: GameState! {
+        didSet {
+            self.currentState.begin()
+        }
+    }
+    private lazy var referee = Referee(gameboard: self.gameboard)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.goToFirstState()
+        
         gameboardView.onSelectPosition = { [weak self] position in
             guard let self = self else { return }
-            self.gameboardView.placeMarkView(XView(), at: position)
+            //self.gameboardView.placeMarkView(XView(), at: position)
+            self.currentState.addMark(at: position)
+            if self.currentState.isCompleted {
+                self.goToNextState()
+            }
+
         }
     }
     
     @IBAction func restartButtonTapped(_ sender: UIButton) {
         
     }
+    
+    private func goToFirstState() {
+        self.currentState = PlayerInputState(player: .first,
+                                             gameViewController: self,
+                                             gameboard: gameboard,
+                                             gameboardView: gameboardView)
+    }
+
+    private func goToNextState() {
+        if let winner = self.referee.determineWinner() {
+            self.currentState = GameEndedState(winner: winner, gameViewController: self)
+            return
+        }
+        
+        if let playerInputState = currentState as? PlayerInputState {
+            self.currentState = PlayerInputState(player: playerInputState.player.next,
+                                                 gameViewController: self,
+                                                 gameboard: gameboard,
+                                                 gameboardView: gameboardView)
+        }
+    }
+
 }
 
